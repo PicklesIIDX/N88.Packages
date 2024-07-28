eeeee6f40b08349a4e027bcf27f99e3b6205109f
auston <auston.montville@gmail.com>
2024-07-28 16:12:42 -0700

    - added N88.Worlds, a simple ECS library

---
+++ N88.Worlds.Spec/Worlds.cs
@@ -0,0 +1,165 @@
+namespace N88.Worlds.Spec;
+
+using FluentAssertions;
+
+public class Worlds
+{
+    private World _world = new();
+    
+    [SetUp]
+    public void Setup()
+    {
+        _world = new World();
+    }
+
+    [Test]
+    public void Create_entities_in_sequential_id_order_starting_from_one()
+    {
+        _world.CreateEntity().Should().Be(1);
+        _world.CreateEntity().Should().Be(2);
+        _world.CreateEntity().Should().Be(3);
+        _world.CreateEntity().Should().Be(4);
+    }
+    
+    [Test]
+    public void Can_only_bind_one_component_of_the_same_type_to_an_entity()
+    {
+        var entity = _world.CreateEntity();
+        _world.TryBindComponentToEntity(entity, new MockComponent()).Should().Be(true);
+        Assert.Throws<ComponentBindException>(() => _world.TryBindComponentToEntity(entity, new MockComponent()));
+    }
+
+    [Test]
+    public void Cannot_bind_to_null_components()
+    {
+        var entity = _world.CreateEntity();
+        Assert.Throws<NullReferenceException>(() => _world.TryBindComponentToEntity<MockComponent>(entity, null!));
+    }
+    
+    [Test]
+    public void Can_return_a_component_bound_to_an_entity()
+    {
+        var entity = _world.CreateEntity();
+        var component = new MockComponent();
+        _world.TryBindComponentToEntity(entity, component).Should().Be(true);
+        _world.TryGetComponentMappedToEntity(entity, out MockComponent? result).Should().Be(true);
+        result.Should().Be(component);
+    }
+    
+    [Test]
+    public void Can_return_a_component_not_not_bound_to_an_entity()
+    {
+        var entity = _world.CreateEntity();
+        var component = new MockComponent();
+        _world.TryBindComponentToEntity(entity, component).Should().Be(true);
+        _world.TryReleaseComponent<MockComponent>(entity).Should().Be(true);
+        _world.GetUnboundComponent<MockComponent>().Should().Be(component);
+    }
+    
+    [Test]
+    public void Will_fail_to_unbind_a_component_not_bound_to_an_entity()
+    {
+        var entity = _world.CreateEntity();
+        _world.TryReleaseComponent<MockComponent>(entity).Should().Be(false);
+    }
+    
+    [Test]
+    public void Will_fail_to_interact_with_non_existent_entity()
+    {
+        var entity = _world.CreateEntity();
+        var nonExistentEntity = entity+1;
+        _world.TryReleaseComponent<MockComponent>(nonExistentEntity).Should().Be(false);
+        _world.TryBindComponentToEntity(nonExistentEntity, new MockComponent()).Should().Be(false);
+        _world.TryGetComponentMappedToEntity(nonExistentEntity, out MockComponent? _).Should().Be(false);
+        _world.TryReleaseEntity(nonExistentEntity).Should().Be(false);
+    }
+    
+    [Test]
+    public void Return_all_entities_bound_to_a_given_component_type()
+    {
+        var entity1 = _world.CreateEntity();
+        _world.TryBindComponentToEntity(entity1, new MockComponent());
+        var entity2 = _world.CreateEntity();
+        _world.TryBindComponentToEntity(entity2, new MockComponent());
+        var entity3 = _world.CreateEntity();
+        _world.TryBindComponentToEntity(entity3, new MockComponent());
+
+        var entities = _world.GetEntitiesWithComponent<MockComponent>();
+        entities.Should().ContainInOrder([entity1, entity2, entity3]);
+    }
+    
+    
+    [Test]
+    public void Can_return_all_components_of_a_type_bound_to_any_entity()
+    {
+        var component1 = new MockComponent();
+        _world.TryBindComponentToEntity(_world.CreateEntity(), component1);
+        var component2 = new MockComponent();
+        _world.TryBindComponentToEntity(_world.CreateEntity(), component2);
+        var component3 = new MockComponent();
+        _world.TryBindComponentToEntity(_world.CreateEntity(), component3);
+
+        var components = _world.GetComponentsForAllEntities<MockComponent>();
+        components.Should().ContainInOrder([component1, component2, component3]);
+    }
+    
+    [Test]
+    public void Disposes_of_disposable_components_when_released_from_an_entity()
+    {
+        var component = new MockComponent();
+        component.Disposed.Should().Be(false);
+
+        var entity = _world.CreateEntity();
+        _world.TryBindComponentToEntity(entity, component);
+        _world.TryReleaseComponent<MockComponent>(entity);
+
+        component.Disposed.Should().Be(true);
+
+        component.Disposed = false;
+        _world.TryBindComponentToEntity(entity, component);
+        _world.TryReleaseEntity(entity);
+
+        component.Disposed.Should().Be(true);
+    }
+
+    [Test]
+    public void Can_recycle_unbound_component()
+    {
+        var entity = _world.CreateEntity();
+        var component = new MockComponent();
+        _world.TryBindComponentToEntity(entity, component).Should().Be(true);
+        _world.TryReleaseComponent<MockComponent>(entity).Should().Be(true);
+
+        _world.GetUnboundComponent<MockComponent>().Should().Be(component);
+    }
+
+    [Test]
+    public void Will_provide_new_component_if_non_unbound_when_requested()
+    {
+        var component = new MockComponent();;
+
+        var newComponent = _world.GetUnboundComponent<MockComponent>();
+        newComponent.Should().NotBe(component);
+        
+        newComponent.Should().BeEquivalentTo((MockComponent)default!);
+
+        var entity = _world.CreateEntity();
+        _world.TryBindComponentToEntity(entity, component).Should().Be(true);
+        _world.TryReleaseEntity(entity).Should().Be(true);
+        var pooled = _world.GetUnboundComponent<MockComponent>();
+        pooled.Should().BeEquivalentTo(component);
+        _world.TryBindComponentToEntity(entity, pooled).Should().Be(true);
+        
+        _world.GetUnboundComponent<MockComponent>().Should().BeEquivalentTo((MockComponent)default!);
+    }
+}
+
+public class MockComponent : IDisposable
+{
+    public bool Disposed = false;
+    public int Count = 0;
+    public void Dispose()
+    {
+        Disposed = true;
+    }
+}
---
+++ N88.Worlds/World.cs
@@ -0,0 +1,172 @@
+﻿namespace N88.Worlds
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// An object to track entities and the components bound to them.
+    /// Supports component pooling.
+    /// </summary>
+    public class World
+    {
+        private readonly Dictionary<Type, Dictionary<int, object>> _components = new();
+        private readonly Dictionary<Type, List<object>> _componentPools = new();
+        private int _idCounter;
+
+        /// <summary>
+        /// Entities are just IDs. Creating one increments the counter.
+        /// Please keep track of your entities.
+        /// </summary>
+        /// <returns>A unique identifier for the entity.</returns>
+        public int CreateEntity()
+        {
+            _idCounter++;
+            return _idCounter;
+        }
+        
+        /// <summary>
+        /// Returns a component that is currently active and mapped to an entity. 
+        /// </summary>
+        public bool TryGetComponentMappedToEntity<T>(int id, out T? component) where T : class
+        {
+            component = default;
+            if (_components.TryGetValue(typeof(T), out var dictionary))
+            {
+                if (dictionary.TryGetValue(id, out var value))
+                {
+                    component = value as T;
+                    return true;
+                }
+            } 
+            return false;
+        }
+
+        /// <summary>
+        /// Binds a component to an entity by id.
+        /// </summary>
+        public bool TryBindComponentToEntity<T>(int id, T component)
+        {
+            if (component == null)
+            {
+                throw new NullReferenceException("Cannot bind to null components");
+            }
+            if (id > _idCounter) { return false; }
+            if (_components.TryGetValue(typeof(T), out var dictionary))
+            {
+                // todo: multiple components of the same type?
+                if (!dictionary.TryAdd(id, component))
+                {
+                    throw new ComponentBindException("no support for multiple components");
+                }
+                return true;
+            }
+            dictionary = new Dictionary<int, object> { { id, component } };
+            _components.Add(typeof(T), dictionary);
+            _componentPools.Add(typeof(T), new List<object>());
+            return true;
+        }
+
+        /// <summary>
+        /// Unbinds all components from an entity id, returning the components to their respective pools,
+        /// and disposing any components that are disposable.
+        /// </summary>
+        public bool TryReleaseEntity(int id)
+        {
+            if (id > _idCounter) { return false; }
+            foreach (var (componentType, componentMap) in _components)
+            {
+                if (!componentMap.TryGetValue(id, out var componentObject)) {continue;}
+                if (componentObject is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+                _componentPools[componentType].Add(componentObject);
+                componentMap.Remove(id);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// If a component of type <see cref="T"/> is not mapped to an entity, it will be returned.
+        /// Otherwise, a default component will be returned. 
+        /// </summary>
+        public T? GetUnboundComponent<T>() where T : class
+        {
+            if (_componentPools.TryGetValue(typeof(T), out var pool))
+            {
+                if (pool.Count == 0)
+                {
+                    return default;
+                }
+                var component = pool[0] as T;
+                pool.RemoveAt(0);
+                return component;
+            }
+            return default;
+        }
+
+        /// <summary>
+        /// Get all components of type <see cref="T"/> bound to any entities. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public IEnumerable<T?> GetComponentsForAllEntities<T>() where T : class
+        {
+            var type = typeof(T);
+            var components = new List<T?>();
+            if (_components.TryGetValue(type, out var map))
+            {
+                foreach (var (_, component) in map)
+                {
+                    components.Add(component as T);
+                }
+            }
+            return components;
+        }
+
+        /// <summary>
+        /// Returns all entities with a component of type <see cref="T"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public IEnumerable<int> GetEntitiesWithComponent<T>() where T : class
+        {
+            var type = typeof(T);
+            var entities = new List<int>();
+            if (_components.TryGetValue(type, out var map))
+            {
+                foreach (var (entity, _) in map)
+                {
+                    entities.Add(entity);
+                }
+            }
+            return entities;
+        }
+
+        /// <summary>
+        /// Releases a binding of a component from the given entity and returns true if
+        /// that is done. Disposes any disposable components that are released and
+        /// returns them to the component pool for reuse.
+        /// </summary>
+        /// <param name="id">Entity id</param>
+        /// <typeparam name="T"></typeparam>
+        public bool TryReleaseComponent<T>(int id) where T : class
+        {
+            var type = typeof(T);
+            if (_components.TryGetValue(type, out var components))
+            {
+                if (components.TryGetValue(id, out var boundComponent))
+                {
+                    _componentPools[type].Add(boundComponent);
+                    components.Remove(id);
+                    if (boundComponent is IDisposable disposable)
+                    {
+                        disposable.Dispose();
+                    }
+
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
