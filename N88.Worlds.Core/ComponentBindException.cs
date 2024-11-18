94a43c6642a3d68112b63faa3a6f1aff3fe44d26
auston <auston.montville@gmail.com>
2024-11-17 21:01:07 -0800

    - removed Worlds project from solution

--- N88.Worlds.Core/ComponentBindException.cs
+++
@@ -1,9 +0,0 @@
-namespace N88.Worlds.Core
-{
-    using System;
-
-    public class ComponentBindException : Exception
-    {
-        public ComponentBindException(string message) : base(message){}
-    }
-}
